7e6cfc88067f3d4ae0b65cee8a89b1373b868367
Alejo20032023 <jmorales.ipedep@gmal.com>
2023-03-23 16:03:26 -0600

    Se agrego

---
+++ Programa 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Programa_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo soy Alejandro");
+        }
+    }
+}
